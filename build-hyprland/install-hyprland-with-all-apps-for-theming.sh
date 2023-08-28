8d9496c3b043d6b322cb64b2b663f5167fee8feb
Bane-AA <bane@angularadvisors.com>
2023-08-29 00:09:04 +0200

    added nwg-look

--- build-hyprland/install-hyprland-with-all-apps-for-theming.sh
+++ build-hyprland/install-hyprland-with-all-apps-for-theming.sh
@@ -182,5 +182,12 @@ ninja -C build
 sudo ninja -C build install
 cd ..
 
+# Clone and install nwg-look
+git clone https://github.com/nwg-piotr/nwg-look.git
+cd swaylock-effects
+make build
+sudo make install
+cd ..
+
 # Clean up
 rm -rf Hyprland swww xdg-desktop-portal-hyprland swappy wl-clipboard pokemon-colorscripts swaylock-effects
