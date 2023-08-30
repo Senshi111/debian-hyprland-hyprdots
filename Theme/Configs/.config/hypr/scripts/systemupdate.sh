e5b3aab8cad59edbbb4b5073d0f423cf79384a2f
Bane-AA <bane@angularadvisors.com>
2023-08-30 23:30:41 +0200

    improve script

--- Theme/Configs/.config/hypr/scripts/systemupdate.sh
+++ Theme/Configs/.config/hypr/scripts/systemupdate.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-# Check release
-if [ ! -f /etc/arch-release ] ; then
+# Check for Debian release
+if [ ! -f /etc/debian_version ] ; then
     exit 0
 fi
 
 # Check for updates
-aur=$(yay -Qua | wc -l)
-ofc=$(pacman -Qu | wc -l)
+sudo apt update > /dev/null
+official_updates=$(apt list --upgradable 2>/dev/null | grep -c upgradable)
+aur_updates=0  # AUR doesn't exist on Debian, so we assume 0 AUR updates
 
 # Calculate total available updates
-upd=$(( ofc + aur ))
-echo "$upd"
+total_updates=$(( official_updates + aur_updates ))
+echo "$total_updates"
 
 # Show tooltip
-if [ $upd -eq 0 ] ; then
+if [ "$total_updates" -eq 0 ] ; then
     echo " Packages are up to date"
 else
-    echo "󱓽 Official $ofc 󱓾 AUR $aur"
+    echo "󱓽 Official $official_updates"
 fi
 
 # Trigger upgrade
 if [ "$1" == "up" ] ; then
-    kitty --title systemupdate sh -c 'yay -Syu'
+    kitty --title systemupdate sh -c 'sudo apt upgrade'
 fi
+
 
