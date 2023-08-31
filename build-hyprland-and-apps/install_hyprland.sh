b3e6aad07cb4469a0b4ad748de9b605df537120b
Bane-AA <bane@angularadvisors.com>
2023-08-31 17:26:56 +0200

    added nvidia patch to hyprland build

--- build-hyprland-and-apps/install_hyprland.sh
+++ build-hyprland-and-apps/install_hyprland.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Clone and build Hyprland
-git clone --recursive https://github.com/hyprwm/Hyprland -b v0.28.0
-cd Hyprland
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland || exit
+
+# Check if an NVIDIA GPU is present
+if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
+    sed 's/glFlush();/glFinish();/g' -i subprojects/wlroots/render/gles2/renderer.c
+fi
+
+# Build Hyprland using Meson and Ninja
 meson build
 ninja -C build
 sudo ninja -C build install
+
+# Return to the previous directory
 cd ..
