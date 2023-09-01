2ae59026c357cb524a0a7776c02a946764cf9635
Bane-AA <bane@angularadvisors.com>
2023-09-01 19:17:22 +0200

    added game launcher

--- Theme/Configs/.config/hypr/scripts/swwwallselect.sh
+++ Theme/Configs/.config/hypr/scripts/swwwallselect.sh
@@ -9,11 +9,17 @@ CurTheme=`gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g"`
 WallPath="$HOME/.config/swww/$CurTheme"
 CacheDir="$HOME/.config/swww/.cache"
 
-# override rofi border
+
+# scale for monitor x res
+x_monres=`cat /sys/class/drm/*/modes | head -1 | cut -d 'x' -f 1`
+x_monres=$(( x_monres*17/100 ))
+
+
+# set rofi override
+
 hypr_border=`awk -F '=' '{if($1~" rounding ") print $2}' $ThemeSet | sed 's/ //g'`
 elem_border=$(( hypr_border * 3 ))
-r_override="element {border-radius: ${elem_border}px;} listview {columns: 6;spacing: 100px;} element {padding: 0px; orientation: vertical;} element-icon {size: 418px; border-radius: 0px;} element-text {padding: 20px;}"
-
+r_override="element{border-radius:${elem_border}px;} listview{columns:6;spacing:100px;} element{padding:0px;orientation:vertical;} element-icon{size:${x_monres}px;border-radius:0px;} element-text{padding:20px;}"
 # launch rofi menu
 RofiSel=$( ls $WallPath | while read rfile
 do
