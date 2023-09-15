ca4916256caae69d557c4671a7e836c89215276b
Bane-AA <bane@angularadvisors.com>
2023-09-15 14:58:16 +0200

    update xdg-desktop-portal-hyprland install

--- build-hyprland-and-apps/install_xdg_portal.sh
+++ build-hyprland-and-apps/install_xdg_portal.sh
@@ -4,10 +4,10 @@
 # Clone and build xdg-desktop-portal-hyprland
 git clone https://github.com/hyprwm/xdg-desktop-portal-hyprland.git
 cd xdg-desktop-portal-hyprland
-meson build --prefix=/usr
-ninja -C build
-# Build hyprland-share-picker
-cd hyprland-share-picker && make all && cd ..
-ninja -C build install
-sudo cp hyprland-share-picker/build/hyprland-share-picker /usr/bin/
+cd subprojects
+git clone https://github.com/hyprwm/hyprland-protocols.git
+git clone https://github.com/Kistler-Group/sdbus-cpp.git
+cd ..
+make all
+sudo make install
 cd ..
