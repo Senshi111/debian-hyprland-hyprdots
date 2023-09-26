0111d3d9122f2751238829996080de813e2e4050
Bane-AA <bane@angularadvisors.com>
2023-09-26 23:20:07 +0200

    update nvidia script

--- build-hyprland-and-apps/install_hyprland.sh
+++ build-hyprland-and-apps/install_hyprland.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Function to add a value to a configuration file if not present
-add_to_modules() {
+add_to_file() {
     local config_file="$1"
     local value="$2"
     
@@ -40,12 +40,24 @@ if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
     add_value="nvidia_drm.modeset=1"
     add_to_grub "$add_value"
 
+    # Define the configuration file and the line to add
+    config_file="/etc/modprobe.d/nvidia.conf"
+    line_to_add="options nvidia-drm modeset=1"
+
+    # Check if the config file exists
+    if [ ! -e "$config_file" ]; then
+        echo "Creating $config_file"
+        sudo touch "$config_file"
+    fi
+
+    add_to_file "$config_file" "$line_to_add"
+
     # Add NVIDIA modules to initramfs configuration
     modules_to_add="nvidia nvidia_modeset nvidia_uvm nvidia_drm"
     modules_file="/etc/initramfs-tools/modules"
     
     if [ -e "$modules_file" ]; then
-        add_to_modules "$modules_file" "$modules_to_add"
+        add_to_file "$modules_file" "$modules_to_add"
         sudo update-initramfs -u
     else
         echo "Modules file ($modules_file) not found."
