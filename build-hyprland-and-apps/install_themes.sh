3422b2523c48e6cd2f776144244a7a2a9b2a0c1d
Bane-AA <bane@angularadvisors.com>
2023-11-23 22:17:17 +0100

    update scripts

--- build-hyprland-and-apps/install_themes.sh
+++ build-hyprland-and-apps/install_themes.sh
@@ -1,5 +1,17 @@
-# install hyprdots files
+#!/bin/bash
 
+# install hyprdots files
+# Clone the repository
 git clone https://github.com/Senshi111/hyprland-hyprdots-files.git
 
-./Theme/Scripts/install.sh
+# Move to the Scripts directory
+cd hyprland-hyprdots-files/Theme/Scripts || exit
+
+# Run the installation script
+./install.sh
+
+# Move back to the original directory
+cd ../../../ || exit
+
+# Optional: Print a message indicating successful completion
+echo "Installation completed successfully."
